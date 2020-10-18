67ddceb2b8bac047249f8ae78090af161d215dc9
IribarneAgustin <agusiri96@gmail.com>
2020-10-18 14:01:31 -0300

    Add genres to movies

--- Views/movie-list.php
+++ Views/movie-list.php
@@ -13,6 +13,7 @@ include('nav-bar.php');
                     <tr>
                         <th style="width: 10%;">Imagen</th>
                         <th style="width: 15%;">Title</th>
+                        <th style="width: 10%;">Genre</th>
                         <th style="width: 15%;">Release Date</th>
                         <th style="width: 10%;">Language</th>
                         <th style="width: 50%;">Overview</th>
@@ -22,7 +23,9 @@ include('nav-bar.php');
                     <?php foreach ($moviesList as $value) { ?>
                         <tr>
                             <?php $id = $value->getId(); ?>
-                            <td> <?php echo '<img src="https://image.tmdb.org/t/p/w220_and_h330_face/' . $value->getImg() . '">' ?> </td>                            <td> <?php echo $value->getTitle(); ?> </td>
+                            <td> <?php echo '<img src="https://image.tmdb.org/t/p/w220_and_h330_face/' . $value->getImg() . '">' ?> </td>                            
+                            <td> <?php echo $value->getTitle(); ?> </td>
+                            <td> <?php echo $value->getGenresName(); ?> </td>
                             <td> <?php echo $value->getReleaseDate(); ?> </td>
                             <td> <?php echo $value->getLanguage(); ?> </td>
                             <td> <?php echo $value->getOverview(); ?> </td>
